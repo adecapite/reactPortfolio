89e61c00c2363cb676e537d7cdee11b4dd33b21c
Anthony DeCapite <anthony.decapite@icloud.com>
2021-06-15 20:23:16 -0400

    started making bio.

---
+++ src/Pages/Bio/bio.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+
+function Bio(){
+    return(
+        <>
+        My name is Anthony DeCapite. I'm a Full Stack Developer using Mongo, JavaScript, React 
+        </>
+    )
+}   
